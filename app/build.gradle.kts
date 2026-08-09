dca73610085dd2b2fc21b5225f8f037b1eb4b064
Capslock800000 <mmzyd@outlook.com>
2026-08-09 18:53:56 +0800

    修复kotlinOption

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,10 +32,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     buildFeatures {
         compose = true
     }
